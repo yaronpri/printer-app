2de8fd490cc1a53334deb03f783fc243e35663be
yaronprigal <yaronprigal@microsoft.com>
2022-07-25 19:07:36 +0300

    v2

--- printer.py
+++ printer.py
@@ -14,11 +14,13 @@ process_sec = int(os.environ.get("PROCESS_SECOND", "30"))
 async def main():
   
   before = datetime.datetime.utcnow()
+  counter = 0
   while True:  
-    current =  datetime.datetime.utcnow()
+    current =  datetime.datetime.utcnow()    
     if (current - before < datetime.timedelta(days=0, hours=0, minutes=0, seconds=process_sec)): 
+      counter = counter + 1
       logger.info(current.strftime("%a, %d %b %Y %H:%M:%S GMT"))    
-      logger.info(current - before)    
+      logger.info("counter = " + str(counter) + " span = " + str(current - before))
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
